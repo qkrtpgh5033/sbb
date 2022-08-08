46bfcb003724d7651aee74494065646ba972cda9
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-08-08 11:39:54 +0900

    Initial commit

---
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/sbb")
+    // 아래 함수의 리턴값을 그대로 브라우저에 표시
+    // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담음.
+    @ResponseBody
+    public String index(){
+        return "안녕";
+    }
+}
