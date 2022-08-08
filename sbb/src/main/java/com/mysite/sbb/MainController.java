c68103ae8e13fbd3b78cfe7dacdb62bf6e64f0dd
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-08-08 14:04:25 +0900

    feat : 문제 추가 # 문제 4 http://localhost:8080/gugudan?dan=4&limit=9 4 * 1 = 1 ... 4 * 9 = 36
    # 문제 5
    http://localhost:8080/mbti?name=홍길동
    INFP
    http://localhost:8080/mbti?name=홍길순
    ENFP
    http://localhost:8080/mbti?name=임꺽정
    INFJ
    http://localhost:8080/mbti?name=본인
    ???

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     static int num = 0;
+
     @RequestMapping("/sbb")
     // 아래 함수의 리턴값을 그대로 브라우저에 표시
     // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답의 바디에 담음.
@@ -70,10 +71,57 @@ public class MainController {
 
     @GetMapping("/increase")
     @ResponseBody
-    public String increase(){
+    public String increase() {
         return """
                 <h1>increase</h1>
                 <h1>%d</h1>
                 """.formatted(num++);
+    }
+    @GetMapping("/problem4")
+    @ResponseBody
+    public String problem4() {
+        return """
+                   <form method="POST" action="/gugudan">
+                    <input type="number" name="a" placeholder= "구구단을 입력해주세요." />
+                    <input type="submit" value="gugudan POST 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String gugudan(Integer dan, Integer limit) {
+        if (dan == null)
+            dan = 9;
+
+        if (limit == null)
+            limit = 9;
+
+        String answer = "";
+        for (int i = 1; i <= limit; i++) {
+            answer += "%d * %d = %d <br>".formatted(dan, i, (dan * i));
+        }
+        return """
+                %s
+                """.formatted(answer);
+    }
+
+    @GetMapping("/mbti")
+    @ResponseBody
+    public String mbti(String name){
+        String mbti = "";
+        if (name.equals("홍길동"))
+            mbti = "INFP";
+        else if (name.equals("홍길순")) {
+            mbti = "ENFP";
+        } else if (name.equals("임꺽정")) {
+            mbti = "INFJ";
+        } else if (name.equals("박범서")) {
+            mbti = "ISFP";
+        }
+        return """
+                <h1>%s</h1>
+                """.formatted(mbti);
+
     }
 }
